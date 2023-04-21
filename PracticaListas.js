960410464a3d6966f0c5a6759e0cf789179f5220
Valentina <vsancris@hotmail.com>
2023-04-21 23:17:03 +0200

    Practicing with JS, Lists, Objects and Dates

---
+++ PracticaListas.js
@@ -0,0 +1,23 @@
+
+//Practica listas
+
+const fecha_nacimiento = new Date(28, 11, 1992);
+
+const libro = {
+    titulo: "hola",
+    autor: "j.k_Rodriguez",
+    fecha: (24, 6, 1995),
+    url: "www.libros.sdhgsjvdjhcbsjdb.com"
+}
+
+const lista = ["valentina", 30, true, fecha_nacimiento, libro ]
+
+console.log(lista);
+
+/*Crea un nuevo archivo JS que contenga una lista 
+con los siguientes elementos:
+- Tu nombre (string)
+- Tu edad (number)
+- ¿Eres desarrollador? (boolean)
+- Tu fecha de nacimiento (Date)
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)*/
